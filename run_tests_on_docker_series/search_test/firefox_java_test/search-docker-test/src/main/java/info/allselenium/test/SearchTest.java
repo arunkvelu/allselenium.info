29e37cafe82e86e41e4cf967af606358bbdbec3e
Arunkumar Velusamy <avelusamy@avelusamy-mbp15.local>
2020-10-30 20:59:36 +0530

    Add sample tests

---
+++ run_tests_on_docker_series/search_test/firefox_java_test/search-docker-test/src/main/java/info/allselenium/test/SearchTest.java
@@ -0,0 +1,49 @@
+package info.allselenium.test;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SearchTest {
+	WebDriver driver;
+
+	@BeforeTest
+	public void openBrowser() {
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.get("http://www.google.com/");
+	}
+
+	@AfterTest
+	public void closeBrowser() {
+		driver.quit();
+	}
+
+	@Test(groups = { "Smoke" })
+	public void testTitle() {
+		String expectedTextTitle = "allselenium.info";
+
+		WebElement searchTextField = driver.findElement(By.xpath("//input[@title='Search']"));
+		searchTextField.sendKeys("allselenium.info");
+		searchTextField.sendKeys(Keys.ENTER);
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.titleContains("Google Search"));
+
+		String actualTitle = driver.getTitle();
+		Assert.assertTrue(actualTitle.contains(expectedTextTitle),
+				String.format("Browser title does not match - Expected text in title: %s, Actual: %s",
+						expectedTextTitle, actualTitle));
+	}
+}
